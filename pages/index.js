ae4ca17969dde2a8877ecfe6ed2b431205d16652
Diana Abyaneh <diana.abyaneh@gmail.com>
2025-09-09 23:48:48 +0330

    add HomePage component with navigation buttons for signup and login

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import styles from "../styles/Home.module.css";
 
 function HomePage() {
+  const router = useRouter();
+  const btnHandler = (event) => {
+    if (event.target.innerText == "ثبت نام") router.push("/signup");
+    else router.push("/login");
+  };
+
   return (
-    <div>HomePage</div>
-  )
+    <div className={styles.container}>
+      <h1>پروژه انبارداری بوت کمپ</h1>
+      <div className={styles.btnContainer}>
+        <button onClick={(e) => btnHandler(e)}>ثبت نام</button>
+        <button onClick={(e) => btnHandler(e)}>ورود</button>
+      </div>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
