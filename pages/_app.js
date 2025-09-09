29c73064d5354adca487b788545577b80281fd4d
Diana Abyaneh <diana.abyaneh@gmail.com>
2025-09-10 02:01:47 +0330

    add toaster to project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import { Toaster } from "react-hot-toast";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster position="top-center" reverseOrder={false} />
+    </>
+  );
 }
