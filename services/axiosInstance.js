f200a4b88bec458a4f31ff89f0ad3a26160903f9
Diana Abyaneh <diana.abyaneh@gmail.com>
2025-09-24 00:56:33 +0330

    fix: ensure token retrieval is only attempted in a browser environment

--- services/axiosInstance.js
+++ services/axiosInstance.js
@@ -5,9 +5,11 @@ const API = axios.create({
 });
 
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
   }
   return config;
 });
